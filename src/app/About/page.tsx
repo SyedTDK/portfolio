39727c5fab781edfed2ed375b196c2fcce705734
SyedTDK <syedfaizullah123@gmail.com>
2023-11-24 13:53:06 -0500

    Contact page Responsive

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -11,7 +11,7 @@ export default async function About() {
                 <h1 className='animate-typing overflow-hidden whitespace-nowrap text-center text-5xl'>About Me</h1>
             </div>
             <div className='flex justify-center p-5'>
-                <p className='text-2xl font-bold backdrop-blur-md'>Hey, I’m Syed and I am an aspiring Frontend developer pursuing a Bachelor of Engineering in Computer Engineering from The City College of New York. 
+                <p className='text-xl md:text-2xl font-bold backdrop-blur-md'>Hey, I’m Syed and I am an aspiring Frontend developer pursuing a Bachelor of Engineering in Computer Engineering from The City College of New York. 
                 I have honed my technical skills through a series of relevant courses and hands-on projects. I am proficient in various programming 
                 languages, including HTML, CSS, JavaScript, TypeScript, and Java and I am familiar with front-end and back-end technologies such as Next.js, React, 
                 Tailwind CSS, Bootstrap, Node.js, Google Cloud, and Firebase. I have applied my expertise in developing both a mental health platform and a fitness app, 
--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -11,7 +11,7 @@ export default async function Contact() {
                 <h1 className='animate-typing overflow-hidden whitespace-nowrap text-center text-5xl'>Contact</h1>
             </div>
             <div className='flex justify-center p-5'>
-                <p className='text-center backdrop-blur-md text-3xl font-bold'>
+                <p className='text-center backdrop-blur-md text-xl md:text-3xl font-bold'>
                     Email: <a href='mailto:syedfaizullah007@gmail.com' target="_blank" className='hover:underline'>syedfaizullah007@gmail.com<span className="material-symbols-outlined">north_east</span></a>
                     <br /><a href='https://www.linkedin.com/in/syed-faizullah/' target="_blank" className='hover:underline hover:'>LinkedIn<span className="material-symbols-outlined">north_east</span></a>
                 </p>
