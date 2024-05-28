6440b9c7054e54a25b0c60a99ab247a21b59890e
SyedTDK <syedfaizullah123@gmail.com>
2024-05-28 14:29:59 -0400

    Update page.tsx

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -12,7 +12,7 @@ export default async function About() {
             </div>
             <div className='flex justify-center p-5'>
                 <p className='text-xl md:text-2xl font-bold backdrop-blur-md text-center'>I’m Syed, a Data Analyst and Frontend developer. 
-                Currently, I'm a Software Engineer at <a href="https://steminstitutenyc.org/" target='_blank'><img src='/SI.svg' height="100" width="100" className='inline'></img></a>. I am proficient in various programming 
+                Currently, I’m a Software Engineer at <a href="https://steminstitutenyc.org/" target='_blank'><img src='/SI.svg' height="100" width="100" className='inline'></img></a>. I’m proficient in various programming 
                 languages, including HTML5, CSS, JavaScript, TypeScript, and SQL and technologies including Next.js, React, 
                 Tailwind CSS, Firebase, and Figma.
                 </p>
